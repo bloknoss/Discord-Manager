e7bf9ed30ffcf68babf3a9ef4ed6c57bfbbfa652
blokclass <acarrod0209@g.educaand.es>
2023-11-08 23:19:31 +0100

    updated

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
-const util = require("util");
-const fs = require("fs");
 const { Client, GatewayIntentBits } = require("discord.js");
 const app = express();
 const port = 3000;
-const tokens = ["MTE3MDgyNTEyNjQwNTczNDQyMA.GD3HDp.FjLUcntQdqNg-53b_rfKwjv9JSVYb7yz1yjU6Q"];
+//const tokens = ["MTE3MDgyNTEyNjQwNTczNDQyMA.GD3HDp.FjLUcntQdqNg-53b_rfKwjv9JSVYb7yz1yjU6Q"];
 
 const logs = require("./logs.json");
+const intents = [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+];
 
-function addLog(message_id, message_content, message_date, user_id, user_name, guild_id, guild_name, channel_id, channel_name) {
+function insertLog(message_id, message_content, message_date, user_id, user_name, guild_id, guild_name, channel_id, channel_name) {
     const sql = `
     INSERT INTO logs (
       message_id,
@@ -21,11 +26,10 @@ function addLog(message_id, message_content, message_date, user_id, user_name, g
       guild_name,
       channel_id,
       channel_name
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ) VALUES ("${message_id}", "${message_content}", ${message_date}, "${user_id}", "${user_name}", "${guild_id}", "${guild_name}", "${channel_id}", "${channel_name}")
   `;
 
-    const values = [message_id, message_content, message_date, user_id, user_name, guild_id, guild_name, channel_id, channel_name];
-    conn.query(sql, values, (error, results) => {
+    conn.query(sql, (error, results) => {
         if (error) {
             console.error("Error inserting data:", error);
         } else {
@@ -34,7 +38,7 @@ function addLog(message_id, message_content, message_date, user_id, user_name, g
     });
 }
 
-function addBot(bot_token, bot_name, bot_avatar, bot_prefix, bot_status, bot_auto, bot_run, user_id) {
+function insertBot(bot_token, bot_name, bot_avatar, bot_prefix, bot_status, bot_auto, bot_run, user_id) {
     const sql = `
     INSERT INTO bots (
       bot_token,
@@ -45,10 +49,10 @@ function addBot(bot_token, bot_name, bot_avatar, bot_prefix, bot_status, bot_aut
       bot_auto,
       bot_run,
       user_id
-    ) VALUES (?, ?, ?, ?, ?, ?, ?)
+    ) VALUES (${bot_token}, ${bot_name}, ${bot_avatar}, ${bot_prefix}, ${bot_status}, ${bot_auto}, ${bot_run}, ${user_id})
   `;
 
-    const values = [bot_token, bot_name, bot_avatar, bot_prefix, bot_status, bot_auto,bot_run, user_id];
+    const values = [bot_token, bot_name, bot_avatar, bot_prefix, bot_status, bot_auto, bot_run, user_id];
     conn.query(sql, values, (error, results) => {
         if (error) {
             console.error("Error inserting data:", error);
@@ -70,35 +74,43 @@ conn.connect((err) => {
     console.log("Connected to DB successfully.");
 });
 
-for (const token of tokens) {
-    const client = new Client({
-        intents: [
-            GatewayIntentBits.DirectMessages,
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildBans,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent,
-        ],
-    });
+const tokens = executeQuery("select bot_token from bots where bot_run is false");
 
-    client.on("ready", () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
+async function startBots() {
+    (async () => {
+        await tokens.then(async (results) => {
+            let stringJSON = JSON.stringify(results[0]);
+            let pureJSON = JSON.parse(stringJSON);
+            let mapped = new Map(Object.entries(pureJSON));
 
-    client.on("messageCreate", (message) => {
-        let message_id = message.id;
-        let message_content = message.content;
-        let message_date = message.createdTimestamp;
-        let user_id = message.author.id;
-        let user = message.author.username;
-        let guild_id = message.guild.id;
-        let guild_name = message.guild.name;
-        let channel_id = message.channel.id;
-        let channel_name = message.channel.name;
-        addLog(message_id, message_content, message_date, user_id, user, guild_id, guild_name, channel_id, channel_name);
-    });
+            mapped.forEach((value, key) => {
+                let token = value;
+                const bot_info = executeQuery(`select * from bots where bot_token = "${token}"`);
+                const client = new Client({
+                    intents: intents,
+                });
 
-    //client.login(token);
+                client.on("ready", () => {
+                    console.log(`Logged in as ${client.user.tag}!`);
+                });
+
+                client.on("messageCreate", (message) => {
+                    let message_id = message.id;
+                    let message_content = message.content;
+                    let message_date = message.createdTimestamp;
+                    let user_id = message.author.id;
+                    let user = message.author.username;
+                    let guild_id = message.guild.id;
+                    let guild_name = message.guild.name;
+                    let channel_id = message.channel.id;
+                    let channel_name = message.channel.name;
+                    insertLog(message_id, message_content, message_date, user_id, user, guild_id, guild_name, channel_id, channel_name);
+                });
+
+                client.login(token);
+            });
+        });
+    })();
 }
 
 app.get("/", (req, res) => {
@@ -106,8 +118,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/logs", (req, res) => {
-    let info = JSON.stringify(logs["messages"], null, 4);
-    res.send("<pre>" + info + "</pre>");
+    let info = executeQuery("select * from logs");
+    (async () => {
+        let messages = [];
+        await info.then(async (results) => {
+            let stringJSON = JSON.stringify(results, null, 4);
+            let pureJSON = JSON.parse(stringJSON);
+
+            messages.push(stringJSON);
+        });
+        res.send("<pre>"+messages+"</pre>");
+    })();
 });
 
 app.listen(port, () => {
@@ -124,14 +145,19 @@ async function executeQuery(query) {
     });
     let [rows, fields] = await conn.execute(query);
     conn.end();
-    return [rows, fields];
+    return rows;
 }
 
-async function myFunction() {
-    const result = await executeQuery("select * from bots where bot_auto is true");
-    for (let token of result[0]) {
-        console.log(token["bot_name"]);
-    }
-}
+let printTokens = async () => {
+    await getTokens.then(async (results) => {
+        let stringJSON = JSON.stringify(results[0]);
+        let pureJSON = JSON.parse(stringJSON);
+        let mapped = new Map(Object.entries(pureJSON));
 
-let x = myFunction();
+        mapped.forEach((value, key) => {
+            console.log(value);
+        });
+    });
+};
+
+startBots();
